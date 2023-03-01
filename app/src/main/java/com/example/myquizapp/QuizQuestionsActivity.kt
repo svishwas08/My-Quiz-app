c12017229fda457473b9d98d4d614c7c674e21bc
svishwas08 <vishwassharma0786@gmail.com>
2023-03-01 08:00:10 +0530

    setting up the question UI And Connecting The UI to the model

--- app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.myquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +11,28 @@ class QuizQuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_question)
 
         val mQuestionsList = Constants.getQuestions()
-        Log.i("questions Size", "${mQuestionsList.size}")
+       Log.i("questions Size", "${mQuestionsList.size}") //for see the question size
+
+
+        var mCurrentPosition: Int = 1
+        val question = mQuestionsList?.get(mCurrentPosition - 1)
+
+
+        progressBar.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition" + "/" + progressBar.getMax()
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+
 
 
     }
+
+
+
+
 }
